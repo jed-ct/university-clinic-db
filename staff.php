fe88b690d3accbca37e7d96f83e40c63bfe4ee81
dedge-ct <tuquerojedric@gmail.com>
2025-11-01 01:23:16 +0800

    fixed patient.php and staff.php mixup

--- staff.php
+++ staff.php
@@ -11,8 +11,8 @@
         <a class="logo" href="./index.php">UPB HSO</a>
         <ul class="links">
             <li><a href="./consultation.php">Consultations</a></li>
-            <li><a href="./doctor.php">Patients</a></li>
-            <li><a href="./patient.php">Staff</a></li>
+            <li><a href="./patient.php">Patients</a></li>
+            <li><a href="./staff.php">Staff</a></li>
             <li><a href="">Contact</a></li>
         </ul>
     </div>
